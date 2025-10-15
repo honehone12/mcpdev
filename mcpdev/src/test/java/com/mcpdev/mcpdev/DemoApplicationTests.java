0ee6a1ec7983a89ae9546e2dd5edca793f999d95
Twelfthonehone <honehone.41games@gmail.com>
2025-10-15 18:52:49 +0900

    init spring

---
+++ mcpdev/src/test/java/com/mcpdev/mcpdev/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcpdev.mcpdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
